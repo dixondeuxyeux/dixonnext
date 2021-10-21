1c7a57f25eb59e49e9e22b286e3c0d37c504f375
dixondeuxyeux <dixondeuxyeux@gmail.com>
2021-10-21 10:17:39 +0000

    slid await in categories

--- pages/api/products/categories.js
+++ pages/api/products/categories.js
@@ -7,8 +7,8 @@ const handler = nc()
 handler.get(async (req, res) => {
   await db.connect()
   const categories = await Product.find().distinct('category')
-  await db.disconnect()
   res.send(categories)
+  await db.disconnect()
 })
 
 export default handler
