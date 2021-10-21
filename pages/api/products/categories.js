e9c04fd94de108a9fae88a0789273d63b5e95a64
dixondeuxyeux <dixondeuxyeux@gmail.com>
2021-10-21 12:07:13 +0000

    res.send(categories)

--- pages/api/products/categories.js
+++ pages/api/products/categories.js
@@ -7,8 +7,8 @@ const handler = nc()
 handler.get(async (req, res) => {
   await db.connect()
   const categories = await Product.find().distinct('category')
-  res.send(categories)
   await db.disconnect()
+  res.send(categories)
 })
 
 export default handler
