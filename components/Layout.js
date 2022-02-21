8f5f83a5dcb01137ed3c3ddf8ff2cd2b98dd5b0d
Martin Dixon <50116491+dixondeuxyeux@users.noreply.github.com>
2022-02-21 18:38:22 +0000

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -341,7 +341,7 @@ export default function Layout({ title, description, children }) {
         </Container>
         <Box component='footer' sx={classes.footer}>
           <Typography sx={classes.lower2a}>
-            All rights reserved. © 2 Dixon / Deux Yeux Photographie
+            All rights reserved. © 2022 Dixon / Deux Yeux Photographie
             <br />
             The Dixon Print Shop offers exhibition quality digital prints for
             collectors and art lovers alike.
