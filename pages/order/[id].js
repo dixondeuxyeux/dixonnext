05417c889e8ee0426e144016f23fec7c267974af
dixondeuxyeux <dixondeuxyeux@gmail.com>
2022-03-18 16:58:50 +0000

    fixed2

--- pages/order/[id].js
+++ pages/order/[id].js
@@ -351,7 +351,7 @@ function Order({ params }) {
                     </Grid>
                     <Grid item xs={6}>
                       <Typography align='right'>
-                        <strong>${totalPrice}</strong>
+                        <strong>${(totalPrice).toFixed(2)}</strong>
                       </Typography>
                     </Grid>
                   </Grid>
