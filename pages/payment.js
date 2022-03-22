57843b20cbf1ad57f005da5a7a4617da7d2a0ef3
dixondeuxyeux <dixondeuxyeux@gmail.com>
2022-03-22 12:36:11 +0000

    removed old node

--- pages/payment.js
+++ pages/payment.js
@@ -65,11 +65,7 @@ export default function Payment() {
                   value='PayPal'
                   control={<Radio />}
                 ></FormControlLabel>
-                {/* <FormControlLabel
-                  label='Stripe'
-                  value='Stripe'
-                  control={<Radio />}
-                ></FormControlLabel> */}
+
                 <FormControlLabel
                   label='Cash'
                   value='Cash'
